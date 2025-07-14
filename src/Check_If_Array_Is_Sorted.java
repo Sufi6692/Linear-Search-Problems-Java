cc076ab84714c069d7af2a2709c37255fc0ca442
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-07-14 20:10:25 +0530

    Check if array is sorted in ascending order
    - Used linear traversal to compare each element with the next
    - If any element is greater than the next, marked array as unsorted
    - Displays sorted version of array using Arrays.sort()
    - Code is modular with separate input/output methods
    - Optimized with early loop exit using 'break' for efficiency

---
+++ src/Check_If_Array_Is_Sorted.java
@@ -0,0 +1,56 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Check_If_Array_Is_Sorted {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int n = input.nextInt();
+        if (n < 0){
+            System.out.println("Kindly enter valued size of an greater than 0.");
+            input.close();
+            return;
+        }
+        int[] array = new int[n];
+        ArrayInput(array,input);
+        ArrayOutput(array);
+        CheckIfArrayIsSorted(array);
+        input.close();
+
+    }
+    static void ArrayInput(int[]array,Scanner input){
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter element in array at index "+i+" : ");
+            array[i] = input.nextInt();
+
+        }
+    }
+    static void ArrayOutput(int[]array){
+        System.out.println(Arrays.toString(array));
+    }
+    static void CheckIfArrayIsSorted(int[]array) {
+
+        boolean ans = true;
+        for (int i = 0; i < array.length - 1; i++) {
+
+            if (array[i] > array[i + 1]) {
+                ans = false;
+                break;
+
+            }
+        }
+        if (ans) {
+            System.out.println("Array is sorted");
+        } else {
+
+            System.out.println("Array is not sorted");
+            System.out.println("Array sorted would be look like this : ");
+            Arrays.sort(array);
+            System.out.println(Arrays.toString(array));
+        }
+
+    }
+}
